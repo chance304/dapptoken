c115cf6b85e782edf591b88c64e269c6cbc366ee
chance304 <chancer.304@gmail.com>
2018-10-03 22:59:33 +0530

    Try for Comment Commit and Push

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.2;
 
 contract DappToken{
+    //checking the total supply of the tokens during initialization
     uint256 public totalSupply;
     constructor () public{
         totalSupply = 1000000;
